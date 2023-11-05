23067a0e660a122470e3704f61d4dec5d9e72009
tigler <tigler777@gmail.com>
2023-11-05 22:34:47 +0700

    add entity-service to public-api

--- projects/common-angular/src/services/public-api.ts
+++ projects/common-angular/src/services/public-api.ts
@@ -2,5 +2,6 @@
  * Public API Surface of common-angular-library
  */
 
+export * from './entity/entity.service';
 export * from './file/file.service';
 export * from './pluralize/pluralize.service';
